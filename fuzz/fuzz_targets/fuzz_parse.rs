d0b4e4c75513c1e5d715b2c5c0ff14942963e52b
Junbeom Lee <junbeoml22@gmail.com>
2024-09-10 20:26:44 +0900

    fuzz_test

--- fuzz/fuzz_targets/fuzz_parse.rs
+++ fuzz/fuzz_targets/fuzz_parse.rs
@@ -6,7 +6,7 @@ use biscuit_converter::BiscuitConverter;
 fuzz_target!(|data: &[u8]| {
     let parser = BiscuitConverter::default();
 
-    if data.iter().all(|&b| b >= 48 && b <= 57) && data.len() < 20 {
+    if data.iter().all(|&b| b >= 48 && b <= 57) && data.len() < 38 {
         let parser = BiscuitConverter::default();
         let _ = parser.to_i128(data);
         let _ = parser.to_u128(data);
