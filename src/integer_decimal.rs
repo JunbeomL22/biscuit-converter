31e968d11f83cc42005445ca20270fc68cbc5d7c
Junbeom Lee <junbeoml22@gmail.com>
2024-09-19 09:16:16 +0900

    nil

--- src/integer_decimal.rs
+++ src/integer_decimal.rs
@@ -1,8 +1,9 @@
 use crate::Biscuit;
+use crate::error::ParseIntErr;
 
 impl Biscuit for i128 {
     #[inline]
-    fn parse_decimal(u: &[u8]) -> Result<Self, crate::error::ParseIntErr> {
+    fn parse_decimal(u: &[u8]) -> Result<Self, ParseIntErr> {
         if !u.is_empty() && u[0] == b'-' {
             u128::unsinged_decimal_core(&u[1..], true, false).map(|val| (!(val as i128)).wrapping_add(1))
         } else {
@@ -12,7 +13,7 @@ impl Biscuit for i128 {
 }
 impl Biscuit for i64 {
     #[inline]
-    fn parse_decimal(u: &[u8]) -> Result<Self, crate::error::ParseIntErr> {
+    fn parse_decimal(u: &[u8]) -> Result<Self, ParseIntErr> {
         if !u.is_empty() && u[0] == b'-' {
             u64::unsinged_decimal_core(&u[1..], true, false).map(|val| (!(val as i64)).wrapping_add(1))
         } else {
@@ -23,7 +24,7 @@ impl Biscuit for i64 {
 
 impl Biscuit for i32 {
     #[inline]
-    fn parse_decimal(u: &[u8]) -> Result<Self, crate::error::ParseIntErr> {
+    fn parse_decimal(u: &[u8]) -> Result<Self, ParseIntErr> {
         if !u.is_empty() && u[0] == b'-' {
             u32::unsinged_decimal_core(&u[1..], true, false).map(|val| (!(val as i32)).wrapping_add(1))
         } else {
@@ -34,7 +35,7 @@ impl Biscuit for i32 {
 
 impl Biscuit for i16 {
     #[inline]
-    fn parse_decimal(u: &[u8]) -> Result<Self, crate::error::ParseIntErr> {
+    fn parse_decimal(u: &[u8]) -> Result<Self, ParseIntErr> {
         if !u.is_empty() && u[0] == b'-' {
             u16::unsinged_decimal_core(&u[1..], true, false).map(|val| (!(val as i16)).wrapping_add(1))
         } else {
@@ -45,7 +46,7 @@ impl Biscuit for i16 {
 
 impl Biscuit for i8 {
     #[inline]
-    fn parse_decimal(u: &[u8]) -> Result<Self, crate::error::ParseIntErr> {
+    fn parse_decimal(u: &[u8]) -> Result<Self, ParseIntErr> {
         if !u.is_empty() && u[0] == b'-' {
             u8::unsinged_decimal_core(u, true, false).map(|val| (!(val as i8)).wrapping_add(1))
         } else {
